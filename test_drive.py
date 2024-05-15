f7894d6c309f961fec812c85d7efe058010b8639
Andile Mhlongo <majiyaz2@gmail.com>
2024-05-15 15:41:41 +0200

    Added testing and training endpoints

--- test_drive.py
+++ test_drive.py
@@ -1,29 +1,47 @@
-from canvas import Canvas
-from racetrack import Track
+import random
 from network import Network
 from evolution import Evolution
 from storage import Storage
-import os
 
-car_image_paths = [os.path.join("images", f"car{i}.png") for i in range(5)]
-canvas = Canvas(Track(4), car_image_paths)
 
-network_dimensions = 5,4,2
-population_count = 4
-max_generation_iterations = 10
-keep_count = 4
+class Tester():
 
-networks = [Network(network_dimensions) for _ in range(population_count)]
-evolution = Evolution(population_count, keep_count)
-storage = Storage("brain.json")
-best_chromosomes = storage.load()
-for c, n in zip(best_chromosomes, networks):
-    n.deserialize(c)
+    def __init__(self):
+        self.network_dimensions = 5,4,2
+        self.population_count = 6
+        self.max_generation_iterations = 10
+        self.keep_count = 6
 
-simulation_round = 1
+        self.networks = [Network(self.network_dimensions) for _ in range(self.population_count)]
+        self.storage = Storage("brain.json")
+        self.best_chromosomes = self.storage.load()
+        self.simulation_round = 1
+        for c, n in zip(self.best_chromosomes, self.networks):
+            n.deserialize(c)
 
-while simulation_round <= max_generation_iterations and canvas.is_simulating:
-    print(f"=== Round: {simulation_round} ===")
-    canvas.simulate_generation(networks, simulation_round)
-    simulation_round +=1
-    
+        
+
+    def test(self):
+
+        while self.simulation_round <= self.max_generation_iterations:
+            print(f"=== Round: {self.simulation_round} ===")
+            self.simulate_generation()
+            self.simulation_round +=1
+
+            print(f"-- Average checkpoint reached: {sum (n.highest_checkpoint for n in self.networks) / len(self.networks):.2f}.")
+            print(f"-- Cars reached goal: {sum (n.has_reached_goal for n in self.networks)} of population {self.population_count}")
+            print(f"-- Average edge distance: {sum (n.smallest_edge_distance for n in self.networks[:self.keep_count]) / self.keep_count:.2f}")
+
+   
+      
+            
+  
+            
+
+    def simulate_generation(self):
+        for network in self.networks:
+            network.highest_checkpoint = random.randint(0, 10)
+            network.smallest_edge_distance = random.uniform(-1.0, 1.0)
+            network.has_reached_goal = random.choice([True, False])
+        
+
